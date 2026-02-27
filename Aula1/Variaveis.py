24f57f800c0046b0a8a1d2f169ba363993440ff6
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-02-27 22:26:41 +0000

    add Variaveis.py

---
+++ Aula1/Variaveis.py
@@ -0,0 +1,44 @@
+'''
+Comentar várias 
+linhas
+'''
+
+#Comentário na mesma linha
+
+#output para consola
+print("Hello World TPSI0226")
+
+#Int
+tel=9 #9 em decimal vale 9
+print(type(tel))
+print(tel)
+
+#Variáveis Dinâmicas alteram de tipo automaticamente
+tel="9" #"9" em string vale 57
+print(type(tel))
+print(tel)
+
+#float
+med=2.3
+print(type(med))
+print(med)
+
+#string
+nom="Claudia"
+print(type(nom))
+print(nom)
+
+#boolean
+flag=True
+print(type(flag))
+print(flag)
+
+#lista []
+lista=[1, "Claudia", 4.4, False, 9]
+print(type(lista))
+print(lista)
+
+#tuples () - Listas que não podem ter seu valor alterado = constantes
+tupla=(1,3,4,6)
+print(type(tupla))
+print(tupla)
