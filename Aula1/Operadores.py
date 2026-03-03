00e275b13993312f7181165887c7f294daa5d8b6
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-03 23:07:00 +0000

    Update Operadores.py

--- Aula1/Operadores.py
+++ Aula1/Operadores.py
@@ -1,60 +1,107 @@
-#Operadores Aritméticos
+'''
+==========================================
+OPERADORES EM PYTHON
+Aritméticos | Decisão | Lógicos | IF | MATCH
+==========================================
+'''
 
-#soma +, sub -, div /, mult *, mode % (resto da div), exponencial **
+# ---------------------------------
+# OPERADORES ARITMÉTICOS
+# ---------------------------------
 
-total=0
-num1=0
-num2=0
+# +  -> soma
+# -  -> subtracção
+# /  -> divisão
+# *  -> multiplicação
+# %  -> resto da divisão (módulo)
+# ** -> potência
 
-#input values
-num1=int(input("Insira num 1"))
-num2=int(input("Insira num 2"))
+num1 = int(input("Insira o número 1: "))
+num2 = int(input("Insira o número 2: "))
 
-total=num1+num2
-print(f"Soma : {total}")
-
-total=num1-num2
-print("Subtração : ", total , ".")
-
-total=num1/num2
-print(f"Divisão : {total}")
-
-total=num1*num2
-print("Multiplicação : ", total , ".")
-
-#Operadores de Decisão
-
-# == igualdade, != diferente, > maior que, < menor que
-# >= maior ou igual e  <= menor ou igual
-
-#expressão
-#val1 == val2   = True/False
-
-#Operadore Lógicos
-
-#and (&&) e or (||)
-
-#expressão
-#val1 > val2 and val2 > val3 = true/false
-#   true     and    false    = false
+print(f"Soma: {num1 + num2}")
+print(f"Subtracção: {num1 - num2}")
+print(f"Divisão: {num1 / num2}")
+print(f"Multiplicação: {num1 * num2}")
+print(f"Resto da divisão: {num1 % num2}")
+print(f"Potência: {num1 ** num2}")
 
 
-#Exercício encontra o maior e o menor
-#IF
+# ---------------------------------
+# OPERADORES DE DECISÃO
+# ---------------------------------
 
-val1=2
-val2=3
-val3=4
+# ==  igualdade
+# !=  diferente
+# >   maior que
+# <   menor que
+# >=  maior ou igual
+# <=  menor ou igual
 
-if val1>val2 and val2>val3:
-    print("val1 é o maior, val3 é o menor")
-elif val2>val1 and val1>val3:
-    print("val2 é o maior, val3 é o menor")
-elif val3 > val1 and val1 > val2:
-    print("val3 é o maior, val1 é o menor")
-elif val1 > val3 and val3 > val2:
-    print("val1 é o maior, val2 é o menor")
-elif val2 > val3 and val3 > val1:
-    print("val2 é o maior, val1 é o menor")
-elif val3 > val2 and val2 > val1:
-    print("val3 é o maior, val1 é o menor")
+
+# ---------------------------------
+# OPERADORES LÓGICOS
+# ---------------------------------
+
+# and -> E (todas as condições verdadeiras)
+# or  -> OU (pelo menos uma verdadeira)
+# not -> negação
+
+
+# ---------------------------------
+# IF SIMPLES
+# ---------------------------------
+
+val1 = 2
+val2 = 3
+val3 = 6
+
+if val1 > val2:
+    print("val1 é maior que val2")
+else:
+    print("val2 é maior ou igual a val1")
+
+
+# ---------------------------------
+# EXERCÍCIO: ENCONTRAR O MAIOR E O MENOR
+# ---------------------------------
+# Forma organizada e correcta
+
+if val1 >= val2 and val1 >= val3:
+    maior = val1
+elif val2 >= val1 and val2 >= val3:
+    maior = val2
+else:
+    maior = val3
+
+if val1 <= val2 and val1 <= val3:
+    menor = val1
+elif val2 <= val1 and val2 <= val3:
+    menor = val2
+else:
+    menor = val3
+
+print(f"O maior valor é: {maior}")
+print(f"O menor valor é: {menor}")
+
+
+# ---------------------------------
+# MATCH CASE (Python 3.10+)
+# ---------------------------------
+
+print("\nMenu:")
+print("1 - Bom dia")
+print("2 - Boa noite")
+print("3 - Sair")
+
+opc = input("Escolha uma opção: ")
+
+match opc:
+    case "1":
+        print("Bom dia")
+    case "2":
+        print("Boa noite")
+    case "3":
+        print("A sair do programa...")
+    case _:
+        print("Opção inválida")
