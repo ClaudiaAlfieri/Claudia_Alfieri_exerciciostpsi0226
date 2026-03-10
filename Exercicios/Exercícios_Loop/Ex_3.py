574735057a8ee21ab3940f771f440ef4bc0b37fd
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-10 17:16:55 +0000

    Create Exercicio_Loop3

---
+++ Exercicios/Exercícios_Loop/Ex_3.py
@@ -0,0 +1,16 @@
+
+#Ler a nota de 10 alunos, calcular a media e mostrar essa média.
+
+nota1=float(input("Qual é o valor da nota do aluno 1?"))
+nota2=float(input("Qual é o valor da nota do aluno 2?"))
+nota3=float(input("Qual é o valor da nota do aluno 3?"))
+nota4=float(input("Qual é o valor da nota do aluno 4?"))
+nota5=float(input("Qual é o valor da nota do aluno 5?"))
+nota6=float(input("Qual é o valor da nota do aluno 6?"))
+nota7=float(input("Qual é o valor da nota do aluno 7?"))
+nota8=float(input("Qual é o valor da nota do aluno 8?"))
+nota9=float(input("Qual é o valor da nota do aluno 9?"))
+nota10=float(input("Qual é o valor da nota do aluno 10?"))
+
+media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7 + nota8 + nota9 + nota10)/10
+print("A média das notas é:", media)
