eb7d5be880de4d2e7325452f24c02935e715b138
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-12 18:52:02 +0000

    Create Exercicio_Loop8

---
+++ Exercicios/Exercícios_Loop/Ex_8.py
@@ -0,0 +1,8 @@
+
+#Faça um algoritmo que gere a seguinte série: 10, 20, 30, 40, ..... 980, 990, 1000.e outro a fazer 15, 25, 35, 985, 995.(dois ciclos)
+
+for i in range(10,1001,10):
+    print(i)
+    
+for i in range(15,996,10):
+    print(i)
