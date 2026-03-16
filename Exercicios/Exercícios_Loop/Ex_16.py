e5a20615d5b092d1d9ba7c6bb1d10b4f555bfb3a
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-16 12:16:50 +0000

    Create Exercicio_Loop16

---
+++ Exercicios/Exercícios_Loop/Ex_16.py
@@ -0,0 +1,18 @@
+
+#Elabore um programa que constitua a média de 30 números pares que sejam introduzidos. Validando a entrada de números inteiros entre 1 e 50.
+
+soma = 0
+contador = 0
+
+while contador < 30:
+    numero = int(input("Digite um número par entre 1 e 50: "))
+
+    if numero >= 1 and numero <= 50 and numero % 2 == 0:
+        soma = soma + numero
+        contador = contador + 1
+    else:
+        print("Número inválido! Tente novamente.")
+
+media = soma / 30
+
+print("A média dos números é:", media)
