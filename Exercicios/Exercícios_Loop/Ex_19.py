b2fb5ae641ae6ba519de49cea3d105f0ad994966
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-03-16 13:11:06 +0000

    Create Exercicio_Loop19

---
+++ Exercicios/Exercícios_Loop/Ex_19.py
@@ -0,0 +1,20 @@
+
+#Escreva um programa que mostre os primeiros 60 números da serie bonatchi.
+# 1, 1, 2, 3, 5, 8, 13, 21.
+# Como se constrói.
+# 1+1=2
+#     1+2=3
+#         2+3=5
+
+a = 1
+b = 1
+
+print(a)
+print(b)
+
+for i in range(60):
+    c = a + b
+    print(c)
+
+    a = b
+    b = c
