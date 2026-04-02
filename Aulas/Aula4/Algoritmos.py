f07c2192e2cdfce1a94d62a993df160bd2e34376
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-04-02 22:12:58 +0100

    Update Algoritmos.py

--- Aulas/Aula4/Algoritmos.py
+++ Aulas/Aula4/Algoritmos.py
@@ -1,36 +1,65 @@
+nomes=["da","fa","oi","da"]
+#index   0    1    2   3
 
-nomes=[]
-
-#1 - Inserir nome
-def insert(nomesi):
-    nomesi.append(input("Digite o nome a inserir: "))
-
-#2 - Listar nome
-def listar(nomesl):
-    for nome in nomesl:
-        print(" Nome: ",nome)
-
-#3 - Eliminar nome
-def delete(nomesd:list):
- nomesd.pop(int(input("Insert posição")))
+indices = []
  
+ #1 - inserir nome
+def insert(nomesi:list):
+    nomesi.append(input("insert um nome"))
+ 
+ #2 - listar nomes
+def listar(nomesl:list):
+    for nome in nomesl:
+        print("nome : " , nome)
+        
+ #3 - delete nome
+def delete(nomesd:list):
+    nomesd.pop( int(input(" insert posiçao ")))
+ 
+ #4 - procurar nome -> retorna a posiçao do nome na lista
+#  def procurar(nomesp:list):
+#     nome=input("insert nome de procura")
+#     for i in range(len(nomesp)):
+#         if nomesp[i] == nome:
+#             print("nome: ",nomesp[i] ," na posiçao ", i)
+
+#4 - procurar nome -> retorna a posiçao do nome na lista e o nome encontrado dentro do while      
+
+def procurar(nomesprocura:list, indices:list):
+    indices.clear()
+    nome = input("Insere um nome: ")
+    for i in range(len(nomesprocura)):
+     if nomesprocura[i] == nome:
+         indices.append(i)
+    return(indices)
+
+
 
 while True:
-    print("1 - Inserir nome")
-    print("2 - Listar nome")
-    print("3 - Eliminar nome")
-    print("4 - Sair")
-    opt=input("Escolha uma opção: ")
+    print ("1 - inserir nome")
+    print ("2 - listar nomes")
+    print ("3 - delete nome")
+    print ("4 - procurar nome")
+    print ("5 - sair")
+    opt=input("Escolha Opção")
     match opt :
         case "1":
             insert(nomes)
-            print(nomes)          
         case "2":
-            listar(nomes)          
+            listar(nomes)
         case "3":
-            delete(nomes)          
+            delete(nomes)
         case "4":
-            print("Fim do programa")
+            #  procurar(nomes)
+            resultado = procurar(nomes, indices)
+            if resultado:
+                for i in resultado:
+                    print(f"Nome '{nomes[i]}' encontrado na posição {i}")     
+                        
+        case "5":
+            print("fim do programa")
             break
-        case _: #Default
-            print(" Não escolheu a opção certa")
+        case _:
+            print("nao escolheu a opçao certa")
+            
+            
