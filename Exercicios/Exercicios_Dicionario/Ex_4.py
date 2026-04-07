a409608a235a9c00151d3dc79174824e271c474a
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-04-07 11:16:58 +0100

    Update Ex_4.py

--- Exercicios/Exercicios_Dicionario/Ex_4.py
+++ Exercicios/Exercicios_Dicionario/Ex_4.py
@@ -5,7 +5,10 @@
 
 utilizador = {'nome': 'Carlos', 'idade': 28}
 
-if "email" in utilizador:
-    print("Email encontrado.")
-else:
-    print("Email não encontrado.")
+def verificar_chave(dicionario, chave):
+    if chave in dicionario:
+        print(f"{chave} encontrado.")
+    else:
+        print(f"{chave} não encontrado.")
+
+verificar_chave(utilizador, "email")
