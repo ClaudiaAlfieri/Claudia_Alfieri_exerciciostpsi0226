b71579c8e550f5c1ce309f0e6fdb4f5338beed5c
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-04-14 11:19:43 +0100

    Update Ex_3.py using while

--- Exercicios/Exercicios_Sort/Ex_3.py
+++ Exercicios/Exercicios_Sort/Ex_3.py
@@ -12,10 +12,13 @@
 
 def ordenar_caracteres(palavra):
     caracteres = list(palavra)
-    for i in range(len(caracteres)):
-        for j in range(i + 1, len(caracteres)):
-            if caracteres[i] > caracteres[j]:  
-                caracteres[i], caracteres[j] = caracteres[j], caracteres[i]
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(caracteres) - 1):
+            if caracteres[i] > caracteres[i + 1]:
+                flag = True
+                caracteres[i], caracteres[i + 1] = caracteres[i + 1], caracteres[i]
     return ''.join(caracteres)
 
 print(ordenar_caracteres("algoritmo"))
