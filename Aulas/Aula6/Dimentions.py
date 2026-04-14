54d00f70d2b32eefa6934c31fafa84c9272e85ea
Claudia Alfieri <claudia.jorge86@gmail.com>
2026-04-14 11:20:29 +0100

    Create Dimentions.py

---
+++ Aulas/Aula6/Dimentions.py
@@ -0,0 +1,13 @@
+
+#index letras 0123456789101112
+listanomes =["Dario Quntal", "João Alves"]
+#index              0              1
+
+print(listanomes[0][0])
+print(listanomes[1][0])
+
+
+#loop 1
+#   loop N
+
+
